0f278e0575104efba5eef1ccc562ab76c10f5f37
Joédyson Bezerra <joedysonbezerra@gmail.com>
2018-08-02 19:20:43 -0300

    like implementado

--- app/routes.js
+++ app/routes.js
@@ -16,4 +16,6 @@ routes.put('/users', controllers.userController.update);
 routes.post('/tweets', controllers.tweetController.create);
 routes.delete('/tweets/:id',controllers.tweetController.destroy)
 
+routes.post('/like/:id', controllers.likeController.toggle);
+
 module.exports = routes;
